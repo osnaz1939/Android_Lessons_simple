860f40677aad62ab872df52e933ab9d83b4a7815
AvmCom <128715058+AvmCom@users.noreply.github.com>
2023-10-13 12:51:41 +0300

    start commit

--- app/src/main/java/android/lessons/simple/MainActivity.kt
+++ app/src/main/java/android/lessons/simple/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
             Android_Lessons_simpleTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    Greeting("Android Lessons Simple")
                 }
             }
         }
@@ -38,6 +38,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     Android_Lessons_simpleTheme {
-        Greeting("Android")
+        Greeting("Android Lessons Simple")
     }
 }
